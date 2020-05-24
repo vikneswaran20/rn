33e0bfaf58bb8a5ec54c6d010035693b35e9909d
vikneswaran <vikneswaran.jayaram@gmail.com>
2020-05-24 23:48:30 +0530

    Updated the error message

--- rn/validation.py
+++ rn/validation.py
@@ -10,6 +10,6 @@ def validate_range(ctx, param, value):
 
 def validate_precision(ctx, param, value):
     if value is not None and value > 10:
-        raise click.BadParameter('The precision should be less than 10')
+        raise click.BadParameter('The precision should be less than or equal to 10')
 
     return value
