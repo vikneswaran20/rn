f3e28de47dbba0006b2e6b9fa4d67942ebd04ae5
vikneswaran <vikneswaran.jayaram@gmail.com>
2020-05-24 23:06:17 +0530

    Fixed the code factor issues

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 [![Build Status](https://travis-ci.org/vikneswaran20/rn.svg?branch=master)](https://travis-ci.org/vikneswaran20/rn)
 [![GitHub](https://img.shields.io/github/license/vikneswaran20/rn?color=%25234cc61e)](https://github.com/vikneswaran20/rn/blob/master/LICENSE)
+[![CodeFactor](https://www.codefactor.io/repository/github/vikneswaran20/rn/badge)](https://www.codefactor.io/repository/github/vikneswaran20/rn)
 
 **rn** is the random value generator command line application.
 
--- rn/validation.py
+++ rn/validation.py
@@ -3,13 +3,13 @@ import click
 
 def validate_range(ctx, param, value):
     if value[0] >= value[1]:
-        raise click.BadParameter('The given range should be positive')
-    else:
-        return value
+        raise click.BadParameter('The given lower range should be lesser than upper range')
+
+    return value
 
 
 def validate_precision(ctx, param, value):
     if value is not None and value > 10:
         raise click.BadParameter('The precision should be less than 10')
-    else:
-        return value
+
+    return value
